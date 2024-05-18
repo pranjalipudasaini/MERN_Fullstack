cc42e7ff8f00f3f5d339d13082385d8e85d54d37
pranjalipudasaini <pudasainipranjali@gmail.com>
2024-05-18 16:10:30 +0545

    Changed env url

--- server/index.js
+++ server/index.js
@@ -2,12 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import cors from "cors";
 import route from "./routes/userRoute.js";
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 dotenv.config();
 
 const PORT = process.env.PORT || 7000; 
